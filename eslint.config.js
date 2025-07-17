507e6cf9502c9e6a62b9b3b18f6ece0d9aad0cc2
ArengDev <kktdell.1@gmail.com>
2025-07-17 22:56:46 +0700

    eslinting

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,33 @@
-import js from "@eslint/js"
-import globals from "globals"
-import reactHooks from "eslint-plugin-react-hooks"
-import reactRefresh from "eslint-plugin-react-refresh"
-import tseslint from "typescript-eslint"
-import { globalIgnores } from "eslint/config"
+const js = require("@eslint/js")
+const globals = require("globals")
+const reactHooks = require("eslint-plugin-react-hooks")
+const reactRefresh = require("eslint-plugin-react-refresh")
+const tseslint = require("typescript-eslint")
+const prettier = require("eslint-plugin-prettier")
+const prettierConfig = require("eslint-config-prettier")
 
-export default tseslint.config([
-    globalIgnores(["dist"]),
+module.exports = tseslint.config([
+    {
+        ignores: ["dist/**"],
+    },
     {
         files: ["**/*.{ts,tsx}"],
         extends: [
             js.configs.recommended,
-            tseslint.configs.recommended,
+            ...tseslint.configs.recommended,
             reactHooks.configs["recommended-latest"],
             reactRefresh.configs.vite,
+            prettierConfig,
         ],
+        plugins: {
+            prettier,
+        },
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
+        },
+        rules: {
+            "prettier/prettier": "error",
         },
     },
 ])
