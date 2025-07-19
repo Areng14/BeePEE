5640b4389c842b0154c74c20a20867273c180642
ArengDev <kktdell.1@gmail.com>
2025-07-19 22:50:49 +0700

    Added description support

--- src/components/items/BasicInfo.jsx
+++ src/components/items/BasicInfo.jsx
@@ -1,6 +1,6 @@
 import { Stack, TextField } from '@mui/material'
 
-function BasicInfo({ name, setName, author, setAuthor }) {
+function BasicInfo({ name, setName, author, setAuthor, description, setDescription }) {
     return (
         <Stack spacing={2}>
             <TextField
@@ -17,6 +17,22 @@ function BasicInfo({ name, setName, author, setAuthor }) {
                 onChange={(e) => setAuthor(e.target.value)}
                 fullWidth
                 variant="outlined"
+            />
+
+            <TextField
+                label="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                fullWidth
+                variant="outlined"
+                multiline
+                sx={{ 
+                    flex: 1,  // This makes it expand
+                    '& .MuiInputBase-root': {
+                        height: '100%',
+                        alignItems: 'flex-start'
+                    }
+                }}
             />
         </Stack>
     )
